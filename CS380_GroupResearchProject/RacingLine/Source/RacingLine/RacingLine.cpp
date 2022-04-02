74cfa97307d3d296eba2488d422699da069c2d92
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-04-02 15:38:55 -0700

    project setting with unreal

---
+++ CS380_GroupResearchProject/RacingLine/Source/RacingLine/RacingLine.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RacingLine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RacingLine, "RacingLine" );
+
+DEFINE_LOG_CATEGORY(LogRacingLine)
+ 
---
+++ CS380_GroupResearchProject/RacingLine/Source/RacingLine/RacingLineGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacingLineGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARacingLineGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARacingLineGameMode();
+};
+
+
+
