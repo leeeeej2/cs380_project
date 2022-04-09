12de8bdc7861fada86d8965531912783fe58c9c8
Hagyeong Kim <hagyeong.kim@digipen.edu>
2022-04-09 04:21:13 -0700

    byeybye

--- CS380_GroupResearchProject/RacingLine/Source/RacingLine/RacingLine.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RacingLine.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RacingLine, "RacingLine" );
-
-DEFINE_LOG_CATEGORY(LogRacingLine)
- 
